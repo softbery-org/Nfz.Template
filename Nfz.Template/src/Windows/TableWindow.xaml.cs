5be889d389c28971b4dc3f209fcd8112aed2fe80
Softbery by Paweł Tobis <softbery@gmail.com>
2023-06-04 19:04:43 +0200

    Start

---
+++ Nfz.Template/src/Windows/TableWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nfz.Template.src.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TableWindow.xaml
+    /// </summary>
+    public partial class TableWindow : Window
+    {
+        public TableWindow()
+        {
+            InitializeComponent();
+            System.Windows.GridLength grid = new System.Windows.GridLength();
+            TemplateTable.Columns.Add(new TableColumn() { Width = GridLength.Auto, Background = BorderBrush });
+
+        }
+    }
+}
